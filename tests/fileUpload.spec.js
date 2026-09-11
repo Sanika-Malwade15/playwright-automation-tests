1933ffc0ba7ef2a75fc8bb8aea8d846961693359
Sanika Mahesh Malwade <malwadesanika6@gmail.com>
2026-09-11 16:49:18 +0530

    verify file upload functionality

---
+++ tests/fileUpload.spec.js
@@ -0,0 +1,17 @@
+const {test, expect}=require("@playwright/test")
+
+test ("Verify file Upload", async ({page})=>{
+     // Single File Upload Verification Test Case 
+    //1. GO to page/site
+    await page.goto("https://the-internet.herokuapp.com/upload")
+    //2. Upload File
+    await page.locator("#file-upload").setInputFiles("./photos/Screenshot (1).png")
+
+    //3.Click on Submit Btn
+    await page.locator("#file-submit").click()
+
+    //4. Get Message for Successful Upload
+    expect( await page.locator("//h3")).toHaveText("File Uploaded!")
+
+
+})
